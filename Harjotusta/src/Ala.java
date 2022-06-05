fee585f4a6a1bb0051e0ecc06f440492841fda26
kattuhkanen <100340603+kattuhkanen@users.noreply.github.com>
2022-06-05 21:54:27 +0300

    Update Ala.java

--- Harjotusta/src/Ala.java
+++ Harjotusta/src/Ala.java
@@ -6,7 +6,7 @@ public class Ala {
 		// TODO Auto-generated method stub
 		Scanner lukija = new Scanner(System.in);
 		
-		System.out.println("Anna ympyr‰n s‰de:");
+		System.out.println("Anna ympyr√§n s√§de:");
 		int sade = lukija.nextInt();
 		
 		double pii = 3.142;
@@ -14,11 +14,10 @@ public class Ala {
 		double ala =  (double)sade * sade;
 		double sateella = ala * pii;
 		
-		System.out.println("Ympyr‰n ala annetulla s‰teell‰: " + sateella);
+		System.out.println("Ympyr√§n ala annetulla s√§teell√§: " + sateella);
 	
 	
+	
+		}
 	}
 
-}
-//Anna ympyr‰n s‰de:3
-//Ympyr‰n ala annetulla s‰teell‰: 28.278
