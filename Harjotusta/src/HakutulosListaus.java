259687fb44efc5451a1b44f079bac6020a758b74
kattuhkanen <100340603+kattuhkanen@users.noreply.github.com>
2022-06-05 22:05:02 +0300

    Update HakutulosListaus.java

--- Harjotusta/src/HakutulosListaus.java
+++ Harjotusta/src/HakutulosListaus.java
@@ -6,15 +6,15 @@ public class HakutulosListaus {
     public static void main(String[] args) {
         List<TavallinenIlmoitus> hakutulokset = new ArrayList<>();
 
-        // Nämä tulokset vastaavat hakua "keittiö":
-        TavallinenIlmoitus matto = new TavallinenIlmoitus("Matto", "Hyvä keittiön matto", "Martta");
-        AsuntoIlmoitus yksioPorvoossa = new AsuntoIlmoitus("Yksiö Porvoossa", "Valoisa yksiö", "Pekka", "Porvoonkatu 1",
+        
+        TavallinenIlmoitus matto = new TavallinenIlmoitus("Matto", "HyvÃ¤ keittiÃ¶n matto", "Martta");
+        AsuntoIlmoitus yksioPorvoossa = new AsuntoIlmoitus("YksiÃ¶ Porvoossa", "Valoisa yksiÃ¶", "Pekka", "Porvoonkatu 1",
                 1, 27);
-        MatkailuvaunuIlmoitus adriaMatrix = new MatkailuvaunuIlmoitus("Adria Matrix", "Hyvä asuntoauto",
+        MatkailuvaunuIlmoitus adriaMatrix = new MatkailuvaunuIlmoitus("Adria Matrix", "HyvÃ¤ asuntoauto",
                 "Asuntoauto Oy", 2020, "bensa", "automaatti");
 
-        // getKuvausteksti on määritelty TavallinenIlmoitus-luokassa, ja se periytyy
-        // kaikille aliluokille
+        
+       
         String kuvaus = yksioPorvoossa.getKuvausteksti();
 
         hakutulokset.add(matto);
@@ -24,8 +24,7 @@ public class HakutulosListaus {
         for (int i = 0; i < hakutulokset.size(); i++) {
             TavallinenIlmoitus ilmoitus = hakutulokset.get(i);
 
-            // toString-metodia kutsutaan automaattisesti aina siitä luokasta, jonka olio
-            // `ilmoitus` on
+            
             System.out.println(ilmoitus.toString());
         }
     }
