2ff12eae9a425656c2df3bfe6dc9880522a60519
kattuhkanen <100340603+kattuhkanen@users.noreply.github.com>
2022-06-05 22:29:06 +0300

    Update Lumi.java

--- Harjotusta/src/Lumi.java
+++ Harjotusta/src/Lumi.java
@@ -6,14 +6,14 @@ public class Lumi {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner lukija = new Scanner(System.in);
-		//boolean LOPPU = false;
+		boolean LOPPU = false;
 		
 		System.out.println("Anna nimi: ");
 		String nimi = lukija.nextLine();
 		
-		//while (true) {
-			//System.out.println("Anna nimi: ");
-			//nimi++;
+		while (true) {
+			System.out.println("Anna nimi: ");
+			nimi++;
 		}
 	}
 
