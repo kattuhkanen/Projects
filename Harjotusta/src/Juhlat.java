0f107f8eaf5d68db50c64754e42433c98cad037a
kattuhkanen <100340603+kattuhkanen@users.noreply.github.com>
2022-06-05 22:18:04 +0300

    Update Juhlat.java

--- Harjotusta/src/Juhlat.java
+++ Harjotusta/src/Juhlat.java
@@ -1,5 +1,3 @@
-package viikko1;
-
 import java.text.DecimalFormat;
 
 import java.util.Scanner;
@@ -14,21 +12,21 @@ public class Juhlat {
 
 		Scanner scanner = new Scanner(System.in);
 
-		System.out.print("Anna juhliin tulevien aikuisten m‰‰r‰: ");
+		System.out.print("Anna juhliin tulevien aikuisten m√§√§r√§: ");
 
-		double aikuistenM‰‰r‰ = scanner.nextDouble();
+		double aikuistenM√§√§r√§ = scanner.nextDouble();
 
 		double pullo = 7;
 
-		double kappaleM‰‰r‰ = Math.ceil(aikuistenM‰‰r‰ / pullo);
+		double kappaleM√§√§r√§ = Math.ceil(aikuistenM√§√§r√§ / pullo);
 
 		DecimalFormat df = new DecimalFormat("0");
 
-		System.out.println("Pulloja tarvitaan " + df.format(kappaleM‰‰r‰) + " kappaletta");
+		System.out.println("Pulloja tarvitaan " + df.format(kappaleM√§√§r√§) + " kappaletta");
 
-		double ylim‰‰r‰inen = pullo - Math.ceil(aikuistenM‰‰r‰ % pullo);
+		double ylim√§√§r√§inen = pullo - Math.ceil(aikuistenM√§√§r√§ % pullo);
 
-		System.out.println("Viimeisest‰ pullosta j‰‰ " + df.format(ylim‰‰r‰inen) + " lasia");
+		System.out.println("Viimeisest√§ pullosta j√§√§ " + df.format(ylim√§√§r√§inen) + " lasia");
 
 		scanner.close();
 
